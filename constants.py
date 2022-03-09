d7b39865e18624e14bb2b75c675d2e1a5d5e8d7f
wbermeo <wbermeo@nexusgeographics.com>
2022-03-09 09:22:51 +0100

    Postgre connection

---
+++ constants.py
@@ -0,0 +1,10 @@
+from unittest.mock import DEFAULT
+
+# file name configuration
+CONFIG_FILE = 'settings.ini'
+# defaults configuration  section
+DEFAULT_SECTION = 'defaults'
+# database configuration section
+DATABASE_SECTION = 'database'
+# database
+POSTGRE = 'postgresql'
--- loader.py
+++ loader.py
@@ -1,7 +1,10 @@
 from configparser import ConfigParser
+import psycopg2
+
+import constants as const
 
 
-def config(filename='settings.ini', defaults_section='defaults'):
+def config(filename, defaults_section):
     # create a parser
     parser = ConfigParser()
     # read config file
@@ -25,7 +28,7 @@ def config(filename='settings.ini', defaults_section='defaults'):
     return (parser, defaults)
 
 
-def db_config(parser, database_section='database'):
+def db_config(parser, database_section):
     """Rescues database configuration from settings file"""
 
     # get section, default to postgresql
@@ -42,6 +45,43 @@ def db_config(parser, database_section='database'):
     return db
 
 
-parser, defaults = config()
-db = db_config(parser, defaults['database'])
-print(db)
+def connect(params, database_name):
+    """ Connect to configured database """
+
+    if database_name == const.POSTGRE:
+        conn = psycopg2.connect(**params)
+
+    return conn
+
+
+conn = None
+
+try:
+    # get file config parse & dict of default configuration
+    parser, defaults = config(const.CONFIG_FILE, const.DEFAULT_SECTION)
+    # get file name from default database configuration
+    db_name = defaults[const.DATABASE_SECTION]
+    # get database section configuration using db name & file parsed
+    params = db_config(parser, db_name)
+    # get a connection using a factory function
+    conn = connect(params, db_name)
+
+    cur = conn.cursor()
+
+    db_version = cur.execute('SELECT version()')
+    db_version = cur.fetchone()
+
+    # display the PostgreSQL database server version
+    print(f'Database connection:\n {db_version}')
+
+    # close the communication with the PostgreSQL
+    cur.close()
+
+except (psycopg2.DatabaseError) as error:
+    print(f"Problem trying to connect to database - '{db_name}'")
+except (Exception) as error:
+    print(error)
+finally:
+    if conn is not None:
+        conn.close()
+        print('DB closed')
